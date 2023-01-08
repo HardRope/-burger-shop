7de2cdf6ded8e2d28590fc1281b930eb711b58ae
HardRope <61661973+HardRope@users.noreply.github.com>
2023-01-08 05:52:33 +0300

    add model Location

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Location(models.Model):
+    address = models.CharField(
+        'Адрес',
+        max_length=200,
+        unique=True,
+    )
+
+    lng = models.DecimalField(
+        'Долгота',
+        max_digits=22,
+        decimal_places=16,
+    )
+    lat = models.DecimalField(
+        'Широта',
+        max_digits=22,
+        decimal_places=16,
+    )
+
+    date = models.DateField(
+        'Дата обновления',
+        default=timezone.now,
+        db_index=True,
+    )
