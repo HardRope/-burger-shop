39cd10737791f03a8b50ec33898727ff7a362ea7
HardRope <61661973+HardRope@users.noreply.github.com>
2023-01-11 02:52:00 +0300

    clean managerfunction

--- foodcartapp/models.py
+++ foodcartapp/models.py
@@ -128,7 +128,7 @@ class RestaurantMenuItem(models.Model):
 
 class OrderQuerySet(models.QuerySet):
     def with_price(self):
-        return self.annotate(order_cost=Sum(F('products__fixed_price') * F('products__quantity'))).order_by('id')
+        return self.annotate(order_cost=Sum(F('products__fixed_price') * F('products__quantity')))
 
 
 class Order(models.Model):
