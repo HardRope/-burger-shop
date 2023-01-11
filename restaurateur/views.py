d4b717be9fa232ab2a2094433615e4d4d8cd6dc9
HardRope <61661973+HardRope@users.noreply.github.com>
2023-01-11 03:00:59 +0300

    replce func to correct app

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -14,7 +14,7 @@ from django.contrib.auth import views as auth_views
 
 from foodcartapp.models import Product, Restaurant, Order
 from locations.models import Location
-
+from locations.utils import fetch_coordinates
 
 class Login(forms.Form):
     username = forms.CharField(
@@ -71,24 +71,6 @@ def get_restaurants(order, restaurants):
                                for product_restaurant in product.menu_items.all() if product_restaurant.availability}
         restaurants = restaurants.intersection(product_restaurants)
     return restaurants
-
-
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lng, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lng, lat
 
 
 @transaction.atomic
