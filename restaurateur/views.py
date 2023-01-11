35b399ac03347ebcb92b305dac4eb99657a3f73e
HardRope <61661973+HardRope@users.noreply.github.com>
2023-01-11 03:17:00 +0300

    clean code

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -87,8 +87,8 @@ def get_location(address, yandex_api_key):
 
 
 def get_distance(location, restaurant_location):
-    order_coordinates = (location.lat, location.lng)
-    restaurant_coordinates = (restaurant_location.lat, restaurant_location.lng)
+    order_coordinates = location.lat, location.lng
+    restaurant_coordinates = restaurant_location.lat, restaurant_location.lng
     return distance.distance(order_coordinates, restaurant_coordinates).km
 
 
