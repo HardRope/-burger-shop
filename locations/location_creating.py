a1a548ab0c580bd6c6b483cf132db782dd5212b0
HardRope <61661973+HardRope@users.noreply.github.com>
2023-01-27 02:44:49 +0300

    remake get_location

--- locations/location_creating.py
+++ locations/location_creating.py
@@ -1,17 +1,14 @@
-from django.db import transaction
-
 from .models import Location
 from .yandex_geocode_api import fetch_coordinates
 
 
-@transaction.atomic
 def get_location(address, yandex_api_key):
-    location, location_created = Location.objects.get_or_create(address=address)
-    if location_created:
-        coordinates = fetch_coordinates(yandex_api_key, address)
-        if coordinates:
-            lng, lat = coordinates
-            location.lng = lng
-            location.lat = lat
-            location.save()
-    return location
+    coordinates = fetch_coordinates(yandex_api_key, address)
+    if coordinates:
+        lng, lat = coordinates
+        location = Location(
+            address=address,
+            lng=lng,
+            lat=lat,
+        )
+        return location
